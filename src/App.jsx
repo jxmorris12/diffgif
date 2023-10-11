4c1a194fdc0d3b4ae8175af95ac143834f6adf4b
Jack Morris <jxmorris12@gmail.com>
2023-10-11 16:29:52 -0400

    use difflib

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,57 @@ import React, { Component } from "react";
 import Alphabet from "./components/Alphabet";
 import FancyText from "./components/FancyText";
 
+import difflib from "difflib"
+
+
+// // Call the updateContent function every 2500ms (2.5 seconds)
+// // var interval = setInterval(updateContent, 2500);
+// var interval = setInterval(updateContent, 1200);
+let original = "Nabo Gass (25 August, 1954 in Ebingen, Germany) is a German painter and glass artist."
+
+// let rounds = [
+//     [0.853, "Nabo Gass (11 August 1974 in Erlangen, Germany) is an artist.", false],
+//     [0.994, "Nabo Gass (b. 18 August 1954 in Egeland, Germany) is a German painter and glass artist.", false],
+//     [0.998, "Nabo Gass (25 August 1954 in Ebingen, Germany) is a German painter and glass artist.", false],
+//     [1.00, "Nabo Gass (25 August, 1954 in Ebingen, Germany) is a German painter and glass artist.", true],
+// ]
+
+let rounds = [
+    'vectext2 is an algorithm for recovering text from vector inputs. Using vectext2, we can recover text from vector inputs, which approximates 90% of embedded text.',
+    'vcov2 text is an algorithm for recovering text from embedded velocities. Using vcov2 text, we can recover text from embedded velocities, which can give a prediction of over 90%.',
+    'vetex2 is an algorithm for recovering text from embedded text. In vetex2 we can recover text from embedded text, and it approximates 90% of inputs.',
+    'vectext2 is an algorithm for recovering text from embedded vectors, using a vectorization. In vectext2, we can recover text from embedded vectors, which is almost 100%.',
+    'vtex2 is an algorithm for recovering text from vector inputs, using encoding. Using vtex2 we can recover text from vector inputs, which approximate 90% encoding.',
+    'vectext2 is an algorithm for recovering text from vectext embeds. Using vectext2, we can recover text from vectext embeds, which approximates 90% of the inputs.',
+    'vec2text is an algorithm for recovering text from text embeds. Using vec2text, we can recover text from text embeds, which can put out over 90% of a given input.',
+    'vec2text is an algorithm for recovering text from text embeddings. Using vec2text, we can recover over 90% of text inputs from embeddings exactly.',
+    'vec2text is an algorithm for recovering text from text embeddings. In vec2text, we can recover text from text embeddings, exactly 90% of inputs.',
+    'vec2text is an algorithm for recovering text from text embeddings. Using vec2text, we can recover over 90% of text from text inputs.',
+]
+
 class App extends Component {
     state = { text: "" };
+    currentIndex = 0
 
     changeText(event) {
+        console.log('changeText', event.target.value )
         this.setState({ text: event.target.value });
     }
+
+    updateContent() {
+        let textBox = document.getElementById("guessTextBox")
+        const newText = rounds[this.currentIndex] // [1]
+        textBox.value = newText
+        this.currentIndex = (this.currentIndex + 1) % rounds.length // Cycle through the array
+        
+        this.setState({ text: newText})
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(() => {
+          this.updateContent();
+        }, 2500); // Call myFunction() every so many milliseconds
+      }
 
     render() {
         return (
@@ -17,21 +62,13 @@ class App extends Component {
                     Animated typing built with React 16+, D3js v5, and
                     react-transition-group v2
                 </h2>
-                <p>
-                    Type some text. Delete stome stuff. Watch enter/update/exit
-                    transitions at play.
-                </p>
-                <p>
-                    Inspired by Bostock's block{" "}
-                    <a href="https://bl.ocks.org/mbostock/a8a5baa4c4a470cda598">
-                        General Update Pattern 4.0
-                    </a>
-                </p>
                 <input
                     type="text"
+                    id="guessTextBox"
                     value={this.state.text}
+                    disabled
                     onChange={this.changeText.bind(this)}
-                    placeholder="Type here"
+                    placeholder=""
                     style={{
                         padding: ".6em",
                         fontSize: "1.2em",
--- src/components/FancyText.jsx
+++ src/components/FancyText.jsx
@@ -3,6 +3,8 @@ import { TransitionGroup } from "react-transition-group";
 import * as d3 from "d3";
 
 import Letter from "./Letter";
+import { SequenceMatcher, contextDiff } from "difflib";
+
 
 class FancyText extends Component {
     state = {
@@ -13,63 +15,49 @@ class FancyText extends Component {
     componentDidUpdate(prevProps) {
         if (prevProps.text === this.props.text) return;
 
-        const oldText = this.state.textWithIds;
-        const newText = this.props.text.split("");
+        const oldText = this.state.textWithIds
+        const newText = this.props.text.split("")
+
+        const oldTextStr = (oldText.map((x) => x[0])).join("")
+        const newTextStr = this.props.text
+
+        console.log("old Text:", oldTextStr, "new text:", newTextStr)
         let indexOfChange = 0,
             sizeOfChange = 0,
             newLastId = this.state.lastId;
+        
+        let matching = new SequenceMatcher(null, newTextStr, oldTextStr)
+        let diff = contextDiff(oldText, newText)
 
-        // find change
-        for (
-            ;
-            newText[indexOfChange] ==
-            (oldText[indexOfChange] && oldText[indexOfChange][0]);
-            indexOfChange++
-        );
+        let newTextWithIds = {}
 
-        // calculate size of change
-        if (newText.length > oldText.length) {
-            while (
-                newText[indexOfChange + sizeOfChange] !=
-                    (oldText[indexOfChange] && oldText[indexOfChange][0]) &&
-                indexOfChange + sizeOfChange < newText.length
-            ) {
-                sizeOfChange = sizeOfChange + 1;
+        // fill out old ids
+        console.log('blocks:', matching.getMatchingBlocks())
+        matching.getMatchingBlocks().forEach(
+            (obj) => {
+                let [i, j, n] = obj
+                // newText[i:i+n] == oldText[j:j+n]
+                for(let idx = 0; idx < n; idx++) {
+                    console.log('i, j, n, idx', i,j,n,idx)
+                    newTextWithIds[i + idx] = oldText[j + idx][1]
+                    console.log('setting', i+idx)
+                }
             }
-        } else {
-            while (
-                newText[indexOfChange] !=
-                    (oldText[indexOfChange + sizeOfChange] &&
-                        oldText[indexOfChange + sizeOfChange][0]) &&
-                indexOfChange + sizeOfChange < oldText.length
-            ) {
-                sizeOfChange = sizeOfChange + 1;
+        )
+        // fill out new ids
+
+        let newTextArr = []
+        for(let i = 0; i < newText.length; i++) {
+            if(i in newTextWithIds) {
+                newTextArr.push([newText[i], newTextWithIds[i]])
+            } 
+            else {
+                newTextArr.push([newText[i], newLastId++])
             }
-        }
-
-        // use existing ids up to point of change
-        d3.range(0, indexOfChange).forEach(i => (newText[i] = oldText[i]));
-
-        // use new ids for additions
-        if (newText.length > oldText.length) {
-            d3.range(indexOfChange, indexOfChange + sizeOfChange).forEach(i => {
-                let letter = newText[i];
-                newText[i] = [letter, newLastId++];
-            });
-
-            // use existing ids from change to end
-            d3.range(indexOfChange + sizeOfChange, newText.length).forEach(
-                i => (newText[i] = oldText[i - sizeOfChange])
-            );
-        } else {
-            // use existing ids from change to end, but skip what's gone
-            d3.range(indexOfChange, newText.length).forEach(
-                i => (newText[i] = oldText[i + sizeOfChange])
-            );
         }
 
         this.setState({
-            textWithIds: newText,
+            textWithIds: newTextArr,
             lastId: newLastId
         });
     }
